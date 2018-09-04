19d39159785e1ed4fe99bbdf3ec5bb662a3d9882
lpw94 <lpw94@qq.com>
2018-09-04 09:56:47 +0800

    啊啊

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
   			<router-link to='/home'>首页</router-link> 
   		</li>
   		<li>
-  			<router-link to='/news'>新闻</router-link> 
+  			<router-link to='/news'>新闻第五位二</router-link> 
   		</li>
   	</ul>
 </template>
